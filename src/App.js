e0f569119acde9f5b2da5d91594b42df654ee882
Soledad Vallejos <soledad.vallejos@arbusta.net>
2022-09-13 01:11:44 -0300

    tp done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logoCat from './imagenes/cat_logo.png';
+import { useState } from 'react'; //Hooks - useState
 
 function App() {
+
+const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = ()=>{
+   setNumClics(numClics + 1); 
+  }
+
+  const reiniciarContador = ()=>{
+    setNumClics(numClics - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img className='logo'
+         src={logoCat}
+        alt ='logo app'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics}/>
+        <Boton
+        texto = 'More Cats'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}/>
+        <Boton
+        texto='Less Cats'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}/>
+      </div>
     </div>
   );
 }
